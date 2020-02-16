4fc2aee57596ef9fda310568676a77125e34aae8
Ramsayleung <ramsayleung@gmail.com>
2020-02-16 14:59:27 +0800

    first commit, add solution of problem 680

---
+++ README.md
@@ -0,0 +1,5 @@
+# Leetcode Problems' Solutions
+
+|   #   |                                   Title                                   |            Solution             | Difficulty |
+| :---: | :-----------------------------------------------------------------------: | :-----------------------------: | :--------: |
+|  680  | [Valid Palindrome II](https://leetcode.com/problems/valid-palindrome-ii/) | [Python](./valid_palindrome.py) |    Easy    |
---
+++ valid_palindrome.py
@@ -0,0 +1,53 @@
+'''
+source: https://leetcode.com/problems/valid-palindrome-ii/
+author: Ramsay leung
+date: 2020-02-16
+
+Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+
+Input: "aba"
+Output: True
+
+Example 2:
+
+Input: "abca"
+Output: True
+Explanation: You could delete the character 'c'.
+
+Note:
+   The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+'''
+
+
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        index = self.isPalindrome(s)
+        chars = list(s)
+        size = len(chars)
+        if (index != -1):
+            firstS = list(chars)
+            del firstS[index]
+            if self.isPalindrome(firstS) == -1:
+                return True
+            lastS = chars
+            del lastS[size - index - 1]
+            return self.isPalindrome(lastS) == -1
+        else:
+            return True
+
+    def isPalindrome(self, chars):
+        """
+        :type chars:list<str>
+        :rtype: int
+        """
+        size = len(chars)
+        for i in range(int(size / 2) + 1):
+            if chars[i] != chars[size - i - 1]:
+                return i
+        return -1
