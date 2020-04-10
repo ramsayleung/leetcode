dbe623343880e3ace01d3ecec62bf1beed44fb2d
Ramsay <ramsayleung@gmail.com>
2020-04-10 12:48:54 +0800

    add solution of problem 113.

---
+++ 100/path_sum_ii.py
@@ -0,0 +1,59 @@
+"""
+source: https://leetcode.com/problems/path-sum-ii/
+author: Ramsay Leung
+date: 2020-04-10
+
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \    / \
+7    2  5   1
+
+Return:
+
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from typing import List
+
+
+# time complexity: O(n), n is the number of nodes of given tree.
+# space complexity: O(n), the maximum value of space is n.
+class Solution:
+    def __init__(self):
+        self.paths = []
+
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        self.helper(root, sum, 0, [])
+        return self.paths
+
+    def helper(self, root: TreeNode, target: int, _sum: int, path: List[int]) -> None:
+        if root:
+            if not root.left and not root.right:
+                if _sum+root.val == target:
+                    path.append(root.val)
+                    self.paths.append(path)
+            else:
+                path.append(root.val)
+                self.helper(root.left, target, _sum+root.val, path.copy())
+                self.helper(root.right, target, _sum+root.val, path)
